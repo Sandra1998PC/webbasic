0a597855aba22dfbccbf9a6eefc8b302f5ef52cd
Sandra1998PC <sandrapctla@gmail.com>
2021-11-15 11:03:52 +0530

    oddevenh

--- javascript/oddevenh.js
+++ javascript/oddevenh.js
@@ -1,7 +1,8 @@
 var num=5
-if(num%2==0){
-    console.log("even")
-}
-else{
-    console.log("odd")
-}
+// if(num%2==0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+console.log(num%2==0?"even":"odd")
